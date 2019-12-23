42ea82b08516cc202fe48031abf15742eb9090fc
Vedamir Efanov <vedamire@protonmail.com>
2019-12-23 19:09:30 +0300

    Formalized test with unittest framework

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,9 @@
 from eosfactory.eosf import *
 import time
+import unittest
 
-def now():
-  return int(time.time())
+
 reset()
-
 create_master_account("master")
 
 create_account("token_host", master, account_name="eosio.token")
@@ -13,46 +12,19 @@ create_account("locktimer", master, account_name="locktimer")
 token = Contract(token_host, "/home/ally/contracts/eosio.contracts/contracts/eosio.token")
 timer = Contract(locktimer, "/home/ally/contracts/locktimer")
 
-# locktimer.set_account_permission(
-#     Permission.ACTIVE, {
-#         "threshold":
-#             1,
-#         "keys": [{"key": locktimer.active_key.key_public, "weight":1 }],
-#         "accounts": [
-#             {
-#                 "permission":
-#                     {
-#                         "actor": locktimer,
-#                         "permission": "active"
-#                     },
-#                 "weight": 1
-#             },
-#             {
-#                 "permission":
-#                     {
-#                         "actor": locktimer,
-#                         "permission": "eosio.code"
-#                     },
-#                 "weight": 1
-#             }
-#         ]
-#     }, Permission.OWNER, (locktimer, Permission.OWNER))
 locktimer.set_account_permission(Permission.ACTIVE, add_code=True)
 token_host.set_account_permission(Permission.ACTIVE, add_code=True)
 
 
-# token.build()
-# timer.build()
+create_account("charlie", master)
+create_account("bob", master)
 
 token.deploy()
 timer.deploy()
 
-create_account("charlie", master)
-create_account("bob", master)
-
 token_host.push_action(
     "create",
-    {
+        {
         "issuer": charlie,
         "maximum_supply": "1000000000.0000 EOS",
         "can_freeze": "0",
@@ -71,42 +43,59 @@ token_host.push_action(
     "transfer",
     {
         "from": charlie, "to": bob,
-        "quantity": "20.0000 EOS", "memo":""
+        "quantity": "50.0000 EOS", "memo":""
     },
     charlie)
+def now():
+    return int(time.time())
+class TestStringMethods(unittest.TestCase):
+    # def setUp(self):
 
-token_host.push_action(
-    "transfer",
-    {
-        "from": charlie, "to": locktimer,
-        "quantity": "0.0150 EOS", "memo":"createtimer"
-    },
-    charlie)
 
-locktimer.table("timerv1", locktimer);
 
-locktimer.push_action(
-    "lock",
-    {
-        "sender": charlie,
-        "id": 0,
-        "receiver": bob,
-        "date": now() + int(6)
-    },
-     permission=(charlie, Permission.ACTIVE)
-)
+    def test_deferred(self):
+        token_host.push_action(
+            "transfer",
+            {
+                "from": charlie, "to": locktimer,
+                "quantity": "0.0150 EOS", "memo":"createtimer"
+            },
+            charlie)
+        locktimer.table("timerv1", locktimer);
 
-time.sleep(10);
-locktimer.table("timerv1", locktimer);
-token_host.table("accounts", bob);
-# locktimer.push_action(
-#     "claimmoney",
-#     {
-#         "receiver": bob,
-#         "id": 0
-#     },
-#      permission=(bob, Permission.ACTIVE)
-# )
-# locktimer.table("timerv1", locktimer);
-# token_host.table("accounts", bob);
+        locktimer.push_action (
+            "lock",
+            {
+                "sender": charlie,
+                "id": 0,
+                "receiver": bob,
+                "date": now() + int(6)
+            },
+             permission=(charlie, Permission.ACTIVE)
+        )
+
+        time.sleep(10);
+        locktimer.table("timerv1", locktimer);
+        token_host.table("accounts", bob);
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    # def teardown(self) {
+    #     token_host.push_action(
+    #         ""
+    #     )
+    # }
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
+
 stop()
